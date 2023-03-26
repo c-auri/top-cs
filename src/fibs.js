eb47e1d7768369ba069f10010190fa652db05250
c-auri <43008483+c-auri@users.noreply.github.com>
2023-03-26 17:54:25 +0200

    Implement fibs

---
+++ src/fibs.js
@@ -0,0 +1,21 @@
+export function iterativeFibs(n) {
+    if (n <= 0) return []
+    if (n === 1) return [ 0 ]
+
+    const result = [ 0, 1 ]
+
+    for (let i = 2; i < n; i++) {
+        result.push(result[i - 2] + result[i - 1])
+    }
+
+    return result
+}
+
+export function recursiveFibs(n) {
+    if (n <= 0) return [ ]
+    if (n === 1) return [ 0 ]
+    if (n === 2) return [ 0, 1 ]
+
+    let previous = recursiveFibs(n - 1)
+    return [...previous, previous.at(-1) + previous.at(-2)]
+}
--- tests/fibs.test.js
+++ tests/fibs.test.js
@@ -1,3 +1,48 @@
-expect('', () => {
+import { iterativeFibs, recursiveFibs } from '../src/fibs'
 
+describe('iterativeFibs returns', () => {
+    describe('the empty array', () => {
+        test('for negative inputs', () => {
+            expect(iterativeFibs(-1)).toEqual([])
+        })
+        test('for 0', () => expect(iterativeFibs(0)).toEqual([]))
+    })
+    describe('the defined starters', () => {
+        expect(iterativeFibs(1)).toEqual([ 0 ])
+        expect(iterativeFibs(2)).toEqual([ 0, 1 ])
+    })
+    describe('the full sequence', () => {
+        expect(iterativeFibs(3)).toEqual([ 0, 1, 1 ])
+        expect(iterativeFibs(4)).toEqual([ 0, 1, 1, 2 ])
+        expect(iterativeFibs(5)).toEqual([ 0, 1, 1, 2, 3 ])
+        expect(iterativeFibs(6)).toEqual([ 0, 1, 1, 2, 3, 5 ])
+        expect(iterativeFibs(7)).toEqual([ 0, 1, 1, 2, 3, 5, 8 ])
+        expect(iterativeFibs(8)).toEqual([ 0, 1, 1, 2, 3, 5, 8, 13 ])
+    })
+})
+
+describe('recursiveFibs returns', () => {
+    describe('the empty array', () => {
+        test('for negative inputs', () => {
+            expect(recursiveFibs(-1)).toEqual([])
+        })
+        test('for 0', () => expect(recursiveFibs(0)).toEqual([]))
+    })
+    describe('the defined starters', () => {
+        expect(recursiveFibs(1)).toEqual([ 0 ])
+        expect(recursiveFibs(2)).toEqual([ 0, 1 ])
+    })
+    describe('the full sequence', () => {
+        expect(recursiveFibs(3)).toEqual([ 0, 1, 1 ])
+        expect(recursiveFibs(4)).toEqual([ 0, 1, 1, 2 ])
+        expect(recursiveFibs(5)).toEqual([ 0, 1, 1, 2, 3 ])
+        expect(recursiveFibs(6)).toEqual([ 0, 1, 1, 2, 3, 5 ])
+        expect(recursiveFibs(7)).toEqual([ 0, 1, 1, 2, 3, 5, 8 ])
+        expect(recursiveFibs(8)).toEqual([ 0, 1, 1, 2, 3, 5, 8, 13 ])
+    })
+    describe('throws RangerError', () => {
+        test('at 7750', () => {
+            expect(() => recursiveFibs(7750)).toThrow(RangeError)
+        })
+    })
 })
