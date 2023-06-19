f44b1cedfaffd338cb38783c58255381770e250b
c-auri <43008483+c-auri@users.noreply.github.com>
2023-06-19 11:23:39 +0200

    Rewrite print function

--- binary-search-tree/print.js
+++ binary-search-tree/print.js
@@ -1,26 +1,28 @@
 import { Tree } from './Tree'
 
-function prettyPrint(node, prefix = "", isLeft = true) {
+const tree = new Tree([ 3, 1, 7, 5 ])
+tree.insert(2)
+tree.insert(6)
+tree.insert(4)
+print(tree)
+
+
+function print(tree) {
+    printNode(tree.root)
+}
+
+function printNode(node, prefix = "", isLeft = true) {
     if (node === null) {
         return
     }
 
     if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
+        printNode(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
     }
 
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`)
 
     if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
+        printNode(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
     }
 }
-
-const tree = new Tree([ 3, 1, 7, 5 ])
-prettyPrint(tree.root)
-tree.insert(2)
-prettyPrint(tree.root)
-tree.insert(6)
-prettyPrint(tree.root)
-tree.insert(4)
-prettyPrint(tree.root)
