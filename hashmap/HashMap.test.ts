0b89989dbc81d02da986f816bff777b92dc7c5ca
cauri <43008483+c-auri@users.noreply.github.com>
2024-08-18 22:03:52 +0200

    implement rudimentary HashMap

---
+++ hashmap/HashMap.test.ts
@@ -0,0 +1,55 @@
+import { HashMap } from "./HashMap.ts"
+
+describe('A newly constructed HashMap', () => {
+  test(`has ${HashMap.initSize} buckets`, () => {
+    expect(new HashMap().numberOfBuckets).toBe(16)
+  })
+  test('has 0 entries.', () => {
+    expect(new HashMap().numberOfEntries).toBe(0)
+  })
+})
+
+describe('Inserting a key value pair', () => {
+  describe('without collision', () => {
+    test('increases numberOfEntries by 1', () => {
+      const map = new HashMap()
+      map.set("key", "value")
+      expect(map.numberOfEntries).toBe(1)
+    })
+    test('makes the value retrievable with get', () => {
+      const map = new HashMap()
+      map.set("key", "value")
+      expect(map.get("key")).toBe("value")
+    })
+  })
+  describe('with collision', () => {
+    test('does not increase numberOfEntries', () => {
+      const map = new HashMap()
+      map.set("key", "old value")
+      map.set("key", "new value")
+      expect(map.numberOfEntries).toBe(1)
+    })
+    test('overwrites the old value', () => {
+      const map = new HashMap()
+      map.set("key", "old value")
+      map.set("key", "new value")
+      expect(map.get("key")).toBe("new value")
+    })
+  })
+})
+
+describe('Trying to get a value', () => {
+  describe('of a previously inserted key', () => {
+    test('returns that value', () => {
+      const map = new HashMap()
+      map.set("key", "value")
+      expect(map.get("key")).toBe("value")
+    })
+  })
+  describe('of a key that does not exist', () => {
+    test('returns null', () => {
+      const map = new HashMap()
+      expect(map.get("key")).toBe(null)
+    })
+  })
+})
---
+++ hashmap/HashMap.ts
@@ -0,0 +1,47 @@
+export class HashMap {
+  static initSize = 16
+  #buckets: string[]
+
+  constructor() {
+    this.#buckets = new Array<string>(16)
+  }
+
+  #hash(key: string) {
+    let result = 0
+    const prime = 31
+
+    for (let i = 0; i < key.length; i++) {
+      result = prime * result + key.charCodeAt(i)
+      result %= this.#buckets.length
+    }
+
+    return result;
+  }
+
+  get numberOfBuckets() {
+    return this.#buckets.length
+  }
+
+  get numberOfEntries() {
+    return this.#buckets.filter(Boolean).length
+  }
+
+  set(key: string, value: string) {
+    const hash = this.#hash(key)
+
+    console.assert(hash > 0 && hash < this.#buckets.length)
+
+    const oldValue = this.#buckets[hash]
+
+    if (oldValue === undefined) {
+      console.log(`Collision for key ${key}, overwriting \"${oldValue}\".`)
+    }
+
+    this.#buckets[hash] = value
+  }
+
+  get(key: string) {
+    const result = this.#buckets[this.#hash(key)]
+    return result === undefined ? null : result
+  }
+}
